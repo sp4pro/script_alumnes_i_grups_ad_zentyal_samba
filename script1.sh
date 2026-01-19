e98ae70656f0b0c2da4ad8292fc64dc9c5c8073e
alumno <alexgamersp4pro@gmail.com>
2026-01-19 20:39:10 +0100

    prueba

--- script1.sh
+++ script1.sh
@@ -4,6 +4,28 @@ set -e
 set -u
 set -o pipefail
 
+1eso() {
 for letra in {a..d}; do
   echo "1eso$letra"
 done
+}
+2eso() {
+for letra in {a..d}; do
+  echo "2eso$letra"
+done
+}
+3eso() {
+for letra in {a..d}; do
+  echo "3eso$letra"
+done
+}
+4eso() {
+for letra in {a..d}; do
+  echo "4eso$letra"
+done
+}
+
+1eso
+2eso
+3eso
+4eso
